a0b2e1eac7253d3c722a13938b7c8b11a4f2e03e
ryandavid-businessnav <ryan.david@businessnav.com>
2022-03-28 11:10:13 +0800

    updates
    updates

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::get('/', [App\Http\Controllers\XeroController::class, 'redirectUserToXero'])->name('redirectUserToXero');
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
